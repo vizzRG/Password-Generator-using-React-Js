2f6418c7e8f8897fbe384a69a0794cb9b6666397
vizzRG <vizzrg@gmail.com>
2024-07-15 21:14:33 +0530

    Done Base

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div className="w-full h-screen bg-black flex justify-center">
-        <div className='w-7 h-7 bg-gray-700 '></div>
+        <div className='w-[40vw] h-[10vh] bg-gray-700 fixed top-8 rounded-lg border border-orange-300'></div>
       </div>
 
     </>
